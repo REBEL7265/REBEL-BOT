a2ef1f6667ccbea2c8af0ee0ab94517a2b0a4513
REBEL7265 <78589233+REBEL7265@users.noreply.github.com>
2021-05-11 21:57:38 +0530

    Update and rename MASTERBOT.py to REBELBOT.py

--- REBELBOT.py
+++ REBELBOT.py
@@ -13,7 +13,7 @@ try:
 except:
   os.system("pip install LEGENDX")
 try:
-  from ULTRA import bot 
+  from REBEL import bot 
 except:
   pass
 from LEGENDX import devs, id, ID
@@ -24,20 +24,20 @@ xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
 import time
 botnickname = os.environ.get("BOT_NICK_NAME")
 ALIVE_NAME = os.environ.get("ALIVE_NAME")
-BOT = str(botnickname) if botnickname else "υℓтяα χ"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
+BOT = str(botnickname) if botnickname else "REBEL BOT"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "REBEL BOT"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-MASTERBOT = "[ULTRA X](https://t.me/MASTERBOTOT)"
+GROUP = "[REBELBOT](https://t.me/REBELBOT_SUPPORT)"
 VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = devs
 ID = ID
 id = id
-REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
+REPO = "[REBELBOT](https://github.com/REBEL725/REBELBOT)"
 
 MASTER = NAME
-GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+GROUP = "[SUPPORT GROUP](https://t.me/REBELBOT_SUPPORT)"
 if __name__=="__main__":
   bot.start()
   bot.run_until_disconnected()
